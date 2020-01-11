4b3272540c7d343429e689c0b84838234a92e847
Mohammad Heidary <heidary_1379@yahoo.com>
2020-01-11 09:52:18 +0330

    add test

--- PatternMatching/Program.cs
+++ PatternMatching/Program.cs
@@ -9,15 +9,15 @@ namespace PatternMatching
     {
         static void Main(string[] args)
         {
-            //GraphMaker.Create(1000, 2000, "../src/graph/");
-            //GraphMaker.Create(4, 3, "../src/pattern/");
+            //GraphMaker.Create(1000, 2000, "../src/star4g/");
+            //GraphMaker.Create(5, 4, "../src/rp/");
             var graphImporter = new Importer();
-            graphImporter.Import("../src/graphTri/");
+            graphImporter.Import("../src/tridom/");
             var expander = new Expander(graphImporter);
 
             var patternImpoter = new Importer();
 
-            var bussiness = new Business(new Pattern(patternImpoter.Import("../src/graphTri/")), expander);
+            var bussiness = new Business(new Pattern(patternImpoter.Import("../src/tri/")), expander);
             bussiness.Run();
             bussiness.PrintResults();
         }
